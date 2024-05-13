76540af55316b405bf799990fc5e3ef7f4be4b40
chao <wuwenchao001@qq.com>
2024-05-13 13:26:26 +0800

    Finish LinkedNodeList

---
+++ src/main/java/com/chao/chapter1/session3/LinkedNodeList.java
@@ -0,0 +1,18 @@
+package com.chao.chapter1.session3;
+
+public class LinkedNodeList {
+    public static void main(String[] args) {
+        LinkedNodeList linkedNodeList = new LinkedNodeList();
+
+
+    }
+
+
+    private class Node{
+        Node next;
+        int data;
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
---
+++ src/main/java/com/chao/chapter1/session3/LinkedNodeStack.java
@@ -0,0 +1,61 @@
+/**
+ * None generic implementation
+ * to upgrade this, use the <Item> item for generics.
+ */
+package com.chao.chapter1.session3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class LinkedNodeStack {
+
+    private Node first = null;
+    private int N = 0;
+
+    private static class Node {
+        Node next;
+        int data;
+    }
+
+    public void push(int data) {
+        Node oldFirst = first;
+        first = new Node();
+        first.data = data;
+        first.next = oldFirst;
+        N++;
+    }
+    public int pop() {
+        int data = 0;
+        if (this.isNotEmpty()) {
+            data = first.data;
+            first = first.next;
+            N--;
+        }
+        return data;
+    }
+    public int peek() {
+        if (this.isNotEmpty()) {
+            return first.data;
+        }
+        return -1;
+    }
+    public int size() {
+        return N;
+    }
+    public boolean isNotEmpty() {
+        return N > 0;
+    }
+
+    public static void main(String[] args) {
+        LinkedNodeStack linkedStack = new LinkedNodeStack();
+        linkedStack.push(1);
+        linkedStack.push(2);
+        linkedStack.push(3);
+
+        StdOut.print(linkedStack.peek());
+
+        for (int i = 0; i < 3; i++) {
+            int popped = linkedStack.pop();
+            StdOut.print(popped);
+        }
+    }
+}
