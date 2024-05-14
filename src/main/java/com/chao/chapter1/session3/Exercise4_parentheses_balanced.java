477534de9a556aedb9aa5f18dd6d74518e900bff
chao <wuwenchao001@qq.com>
2024-05-14 23:00:35 +0800

    update 1.3.4

--- src/main/java/com/chao/chapter1/session3/Exercise4_parentheses_balanced.java
+++ src/main/java/com/chao/chapter1/session3/Exercise4_parentheses_balanced.java
@@ -3,15 +3,18 @@ package com.chao.chapter1.session3;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Exercise4ParenthesesBalanced {
+public class Exercise4_parentheses_balanced {
     public static void main(String[] args) {
-        // due to avoid using static modifier for all method, use new Class since 1.3.4
-        Exercise4ParenthesesBalanced exercise = new Exercise4ParenthesesBalanced();
-        StdOut.println("Check '{1{2[221(3)]}}', expected true: " + exercise.isBalanced("{1{2[221(3)]}}"));
-        StdOut.println("Check '{[(9}]}', expected false: " + exercise.isBalanced("{[(9}]}"));
+        // Due to avoid using static modifier for all method, use new Class since 1.3.4.
+        // But, is it meaningful when use instance method to test an outside String? No.
+        // So, just use static modifier for method algorithms, use public modifier for class algorithms.
+//        Exercise4_ParenthesesBalanced exercise = new Exercise4_ParenthesesBalanced();
+
+        StdOut.println("Check '{1{2[221(3)]}}', expected true: " + isBalanced("{1{2[221(3)]}}"));
+        StdOut.println("Check '{[(9}]}', expected false: " + isBalanced("{[(9}]}"));
     }
 
-    public boolean isBalanced(String s) {
+    public static boolean isBalanced(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
